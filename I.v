71931f1b67500cecc1b180da5d3f296a285cea2a
Christopher Mazey <cjmazey@gmail.com>
2015-05-04 20:41:30 -0400

    I2

--- I.v
+++ I.v
@@ -5,5 +5,21 @@ Proof.
   intros a_imp_b b_imp_c.
   intros a.
   refine (b_imp_c _).
-  refine (a_imp_b _).
-  exact a.  Qed.
+    refine (a_imp_b _).
+      exact a.
+Qed.
+
+Theorem I2 : forall A B C : Prop,
+    ((A \/ B) -> C) -> ((A -> C) /\ (B -> C)).
+Proof.
+  intros A B C.
+  intros a_or_b_imp_c.
+  refine (conj _ _).
+    intros a.
+    refine (a_or_b_imp_c _).
+      exact (or_introl a).
+
+    intros b.
+    refine (a_or_b_imp_c _).
+      exact (or_intror b).
+Qed.
